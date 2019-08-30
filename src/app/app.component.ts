aa9439b99fac880d14b3ec9e6337e626e44ffa74
itsdeepak8400 <deepakverma91123@gmail.com>
2019-08-30 07:37:50 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-
+import {ChangeDetectorRef, OnDestroy} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  mobileQuery: MediaQueryList;
 
-   mar: 'my name';
+  
 
+ 
+  private _mobileQueryListener: () => void;
 
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,65 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatListModule} from '@angular/material/list';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatRippleModule} from '@angular/material/core';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { HomeComponent } from './screen/home/home.component';
+import { AboutComponent } from './screen/about/about.component';
+import { PrivacyComponent } from './screen/privacy/privacy.component';
+import { ContactComponent } from './screen/contact/contact.component';
+import { IntroComponent } from './screen/intro/intro.component';
+
+
+
+
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PrivacyComponent,
+    ContactComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,BrowserAnimationsModule,MatToolbarModule
+    AppRoutingModule,BrowserAnimationsModule,MatToolbarModule,MatAutocompleteModule,MatCheckboxModule,MatDatepickerModule,
+    MatFormFieldModule,MatInputModule,MatRadioModule,MatSelectModule,MatSliderModule,MatSlideToggleModule,MatMenuModule,
+    MatSidenavModule,MatCardModule,MatDividerModule,MatExpansionModule,MatGridListModule,MatListModule,MatStepperModule,
+    MatTabsModule,MatButtonModule,MatButtonToggleModule,MatBadgeModule,MatChipsModule,MatIconModule,MatProgressSpinnerModule,
+    MatProgressBarModule,MatRippleModule,MatBottomSheetModule,MatDialogModule,MatSnackBarModule,MatTooltipModule
     
   ],
   providers: [],
