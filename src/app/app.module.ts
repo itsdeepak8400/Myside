8740b8c53c2e61234e81aa27ce00830a9d91c3e2
itsdeepak8400 <deepakverma91123@gmail.com>
2019-08-30 06:18:43 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,BrowserAnimationsModule,MatToolbarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
